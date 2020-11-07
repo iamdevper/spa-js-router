45c0b5d924c95300ae72579fdf996a12a934413b
Moov Space <moovspace@gmail.com>
2020-11-07 16:22:00 +0100

    SPA page component js
    SPA page component js

--- components/page3.js
+++ components/page3.js
@@ -4,7 +4,13 @@ class Page extends Component
 {	
 	Setup(div, data = []) 
 	{		
-		console.log("Component params ", data);
+		// GET request params
+		let id = this.queryParam('id');
+		let name = this.queryParam('name');
+
+		document.title = 'Page 3';        
+
+		console.log("Component params ", data, 'GET id', id, 'GET name', name);
 
 		let m = document.querySelector(div)
 		let html = '<h1 id="boo-click"> Welcome on page 3! Click here! </h1> <p>' + location.pathname + '</p>'
