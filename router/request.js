bcded2b8aad697935b94adc1ff38ded4f0c429e6
boo <boo@boo.xx>
2021-04-28 16:24:16 +0200

    Js fetch data class

---
+++ router/request.js
@@ -0,0 +1,98 @@
+class Request
+{
+    method = 'GET'
+    cors = 'same-origin'
+    credentials = 'same-origin'
+	headers = new Headers()
+	isJsonRequest = false
+
+    Method(str = 'GET')
+    {
+        this.method = str
+        return this
+    }
+
+    Cors(str = 'cors')
+    {
+        // (*) cors, no-cors, same-origin
+        this.cors = str
+        return this
+    }
+
+    Credentials(str = 'same-origin')
+    {
+        // (*) same-origin, include, omit
+        this.credentials = str
+        return this
+    }
+
+    Headers(obj = {})
+    {
+        this.headers = obj
+        return this
+	}
+
+	Header(name, value)
+	{
+		this.headers.append(name, value)
+		return this
+	}
+
+	Json()
+	{
+		this.headers.append('Content-type', 'application/json; charset=UTF-8')
+		this.isJsonRequest = true
+		return this
+	}
+
+    async Send(url, formData)
+    {
+        let opt = {
+            method: this.method,
+            mode: this.cors,
+            credentials: this.credentials,
+            headers: this.headers
+        }
+		opt.body = formData
+		return await (await fetch(url, opt)).text()
+    }
+
+    async SendJson(url, obj)
+    {
+		this.Json()
+
+        let opt = {
+            method: this.method,
+            mode: this.cors,
+            credentials: this.credentials,
+            headers: this.headers
+        }
+		opt.body = JSON.stringify(obj)
+        return await (await fetch(url, opt)).json()
+    }
+}
+
+class Cors
+{
+    static get Cors() { return 'cors' }
+    static get NoCors() { return 'no-cors' }
+    static get SameOrigin() { return 'same-origin' }
+}
+
+class Credentials
+{
+    static get Include() { return 'include' }
+    static get Omit() { return 'omit' }
+    static get SameOrigin() { return 'same-origin' }
+}
+
+export { Request as default, Cors, Credentials };
+
+/*
+new Req().Method('GET')
+.Json().Cors()
+.SendJson('https://jsonplaceholder.typicode.com/users/3/posts')
+.then((s) => {
+	console.log(s)
+});
+*/
